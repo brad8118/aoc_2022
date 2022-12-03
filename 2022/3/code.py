f0998dbc0e5715173797e9ec6e0d4d44cad3283c
Brad - Basement pc <brad8118@gmail.com>
2022-12-03 06:50:17 -0500

    day3 part1 30m

--- 2022/3/code.py
+++ 2022/3/code.py
@@ -2,13 +2,45 @@
 # Author = ?
 # Date = December 2018
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+class AOC:
+    def __init__(self):
+        self.letters = []
+        self.totalpriority = 0
+        self.letterValue = {}
+
+        for i, l in enumerate("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+            self.letterValue[l] = i+1
+
+    def readFile(self, file):
+        with open(dataFile) as file:
+            while (line := file.readline().rstrip()):
+                lookup = {}
+                halfed = len(line)/2
+                for i, l in enumerate(line):
+                    if i < halfed:
+                        lookup[l]=''
+                    elif l in lookup:
+                        # print(l, self.letterValue[l])
+                        self.totalpriority += self.letterValue[l]
+                        break
 
 
+    def part1(self): # 6:20 -> 6:50 : 30m
+        print("Part One : ", self.totalpriority)
 
-print("Part One : "+ str(None))
+    def part2(self): # 11:30 -> 12 -> 30m
+        part2Total = 0
+        # for game in self.matches:
+        #     game.updateMySelectionForPart2()
+        #     game.calculateScore()
+        #     part2Total += game.score
+        #     # print(game)
+        print("Part Two : ", part2Total)
 
 
-
-print("Part Two : "+ str(None))
+aoc = AOC()
+dataFile = "input.txt"
+# dataFile = "test1.txt"
+aoc.readFile(dataFile)
+aoc.part1()
+aoc.part2()
