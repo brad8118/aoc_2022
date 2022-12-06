9f3f014184a41c28fbea52cd55bcc064029fb3eb
Brad <brad8118@gmail.com>
2022-12-06 08:56:09 -0500

    day6 part2 8m)

--- 2022/6/code.py
+++ 2022/6/code.py
@@ -28,6 +28,19 @@ class AOC:
             lookup = lookup[1:4] + self.dataStream[i]
         print("Part One : ", self.key, self.part1_index)
 
+    def part2(self): #8:47 =>  8:54 => 8m
+        lookup = self.dataStream[0:14]
+        # print(self.dataStream)
+        for i in range(14, len(self.dataStream)):
+            # print(lookup, i, self.dataStream[i])
+            if len(set(lookup)) == len(lookup):
+                self.key = lookup
+                self.part1_index = i
+                break
+            
+            lookup = lookup[1:14] + self.dataStream[i]
+        print("Part One : ", self.key, self.part1_index)
+
     # def part1(self): #7:35 ->
     #     lookup = OrderedDict()
     #     lookup[self.dataStream[0]] = 0
@@ -55,32 +68,10 @@ class AOC:
 
 
 
-    def part2(self, file): # 
-        self.totalpriority = 0
-        grouping = []
-
-        with open(dataFile) as file:
-            while (line := file.readline().rstrip()):
-                lookup = {}
-                for l in line:
-                    lookup[l] = True                    
-                
-                grouping.append(lookup)
-
-                if len(grouping) == 3:
-                    for key in grouping[0]:
-                        if key in grouping[1] and key in grouping[2]:
-                            self.totalpriority += self.letterValue[key]
-                            grouping = []
-                            break
-            
-        print("Part Two : ", self.totalpriority)
-
-
 aoc = AOC()
 dataFile = "input.txt"
 # dataFile = "test1.txt"
 aoc.readFile(dataFile)
 # aoc.dataStream = "nppdvjthqldpwncqszvftbrmjlhg"
 aoc.part1()
-# aoc.part2(dataFile)
+aoc.part2()
